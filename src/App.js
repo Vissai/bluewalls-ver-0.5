a749d0446aa24453b69b1d7c81a15b3bc8838c51
zubae <afroze@outlook.in>
2021-05-07 09:54:37 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/Header/Header';
+
 import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         Hello World!
       </div>
     )
---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button, Container, FormControl, InputGroup, Navbar} from 'react-bootstrap'
+
+import './Header.css';
+
+const Header = () => {
+    /* 
+    * Responsiveness Ok till Tab
+    */
+    return (
+        <Navbar>
+            <Container>
+                <div className='nav-wrap w-100 m-3'>
+                    <div className='brand-wrap'><img src='/assets/images/bluewallslogo_main.png' alt='mainlogo'/></div>
+                    <div className='navbar-group'>
+                        <div className='mx-1'><InputGroup size='sm'><FormControl placeholder='Username' /></InputGroup></div>
+                        <div className='mx-1'><InputGroup size='sm'><FormControl placeholder='Password' /></InputGroup></div>
+                        <div className='mx-1'><Button variant='primary' size='sm'>Log In</Button></div>
+                    </div>
+                </div>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
